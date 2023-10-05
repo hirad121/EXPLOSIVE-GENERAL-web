e047d80e0da370976404f0fe43f3442e5f9ba75f
Elliott Goubeaux <126128333+egoubeaux@users.noreply.github.com>
2023-10-05 18:04:30 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="grid-container">
+        <header class="header">
+            <div class="brand">
+                <button onclick="openMenu()">&#9776;</button>
+                <a href="index.html">Jet Piranha</a>
+        </div>
   );
 }
 
