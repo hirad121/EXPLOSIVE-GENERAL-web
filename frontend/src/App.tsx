6cc7e891f37cfc894e847108f3627c5df2cee519
Elliott Goubeaux <126128333+egoubeaux@users.noreply.github.com>
2023-10-05 18:17:08 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const openMenu = () => {
+document.querySelector(“.sidebar”)?.classList.add(“open”);
+}
+
+const openMenu = () => {
+document.querySelector(“.sidebar”)?.classList.remove(“open”);
+}
+
   return (
     <div className="grid-container">
         <header className="header">
             <div className="brand">
-                <button onclick="openMenu()">&#9776;</button>
+                <button className="sidebar-close-button" onClick={closeMenu}>
                 <a href="index.html">Explosive General</a>
         </div>
   );
