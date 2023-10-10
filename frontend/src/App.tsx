bba22f36a57bf4c808f0527162a84c9c7bfdd637
egoubeaux <goubeauxe@yahoo.com>
2023-10-10 19:07:26 -0400

    This version has a bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./Home/Home";
+import Products from "./Products/Products";
+
 
 function App() {
   
@@ -13,23 +17,24 @@ function App() {
     }
 
   return (
+    <Router>
+      <div className="App">
     <div className="grid-container">
+    <link rel="stylesheet" href="style.css" />
+	   <title>Explosive General</title>
+    
     <header className="header">
         <div className="brand">
             <button onClick={openMenu}>&#9776;</button>
             <a href="index.html">Explosive General</a>
     </div>
     <div className="header-links">
-        <a href="cart.html">Cart</a>
-        <a href="signin.html">Sign In</a> 
+    <Link to="/">Home</Link>
+	  <Link to="/catalog">Catalog</Link>
         </div>
     </header>
 
-<head>
-    <title>Explosive General</title>
-    <link rel="stylesheet" href="style.css"/>
-</head>
-<body>
+
 <div>
     <header>
         Explosive General
@@ -48,63 +53,21 @@ function App() {
             </li>
             </ul>
             </aside>        
-    <main>
-        <div className="content">
-            <ul className="products">
-            <li>
-            <div className="product">
-            <img className="product-image" src="images/d1.jpg" alt="product" />
-            <div className="product-name">
-            <a href="product.html">Slim Shirt</a>
-            </div>
-            <div className="product-brand">Nike</div>
-            <div className="product-price">$60</div>
-            <div className="product-rating">4.5 Stars (10 Reviews)</div>
-            </div>
-            </li>
-            <li>
-                <div className="product">
-                <img className="product-image" src="images/d1.jpg" alt="product" />
-                <div className="product-name">
-                <a href="product.html">Slim Shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                </div>
-                </li>
-                <li>
-                    <div className="product">
-                    <img className="product-image" src="images/d1.jpg" alt="product" />
-                    <div className="product-name">
-                    <a href="product.html">Slim Shirt</a>
-                    </div>
-                    <div className="product-brand">Nike</div>
-                    <div className="product-price">$60</div>
-                    <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                    </div>
-                    </li>
-                    <li>
-                        <div className="product">
-                        <img className="product-image" src="images/d1.jpg" alt="product" />
-                        <div className="product-name">
-                        <a href="product.html">Slim Shirt</a>
-                        </div>
-                        <div className="product-brand">Nike</div>
-                        <div className="product-price">$60</div>
-                        <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                        </div>
-                        </li>
-            </ul>
-            </div>
-            
-    </main>
+    
+    <main className="main">
+     <Routes>
+       <Route path="/" element={<Home />} />
+       <Route path="/catalog" element={<Products />} />
+     </Routes>
+</main>
     <footer>
         &copy; 2023 Explosive General
     </footer>
     </div>
-</body>
+
 </div>
+</div>
+  </Router>
   );
 }
 
